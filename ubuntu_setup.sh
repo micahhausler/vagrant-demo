c0d16ce31742e291d9310a1ab82cac6383c601e4
Micah Hausler <micah.hausler@akimbo.io>
2013-12-03 15:40:24 -0500

    Updated documentation of what is going on

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7,11 +7,14 @@ then
     sudo apt-get -y install htop wget curl multitail git vim bash-completion zip unzip;
     sudo apt-get -y install python-pip python-virtualenv python-dev libpq-dev libzmq-dev;
 
-    # Activate the virtualenv
+    # Create the virtualenv
     virtualenv venv;
+    # activate the virtualenv
     . ./venv/bin/activate
-    pip install uwsgi ipython tornado werkzeug jinja2;
+    # install ipython and dependencies into virtualenv
     pip install pyzmq matplotlib -b /tmp;
+    pip install uwsgi tornado werkzeug jinja2;
+    pip install ipython;
 
     touch /home/vagrant/.setup_complete;
 fi
